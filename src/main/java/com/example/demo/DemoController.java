03b0f0b5e3f7cb81bfa8ebed40184ae0fe320ef3
k-furuya-interspace <82435667+k-furuya-interspace@users.noreply.github.com>
2022-04-12 16:27:27 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping(value="hello")
+    public String hello(){
+        return "hello world";
+    }
+
+    @GetMapping(value="kon")
+    public String kon(){
+        return "kon";
+    }
+}
