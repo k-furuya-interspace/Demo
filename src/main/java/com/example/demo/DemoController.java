81da5838ed3e3080622ab0c37527e488d948ac70
k-furuya-interspace <82435667+k-furuya-interspace@users.noreply.github.com>
2022-04-12 17:15:28 +0900

    Update DemoController.java

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -15,4 +15,10 @@ public class DemoController {
     public String kon(){
         return "kon";
     }
+
+    @GetMapping(value="sayounara")
+    public String sayounara(){
+        String aiueo = "aiueo";
+        return "sayounara";
+    }
 }
